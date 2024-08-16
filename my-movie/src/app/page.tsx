4e34f61f0a07b6722c8a8735beb3953058017df7
walleenates <120362226+walleenates@users.noreply.github.com>
2024-08-16 08:53:28 +0000

    wwall

---
+++ my-movie/src/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    
+    <main>
+      <nav className="navbar">
+
+        <h1>WeMovies</h1>
+    <p> <a href="https://ph.linkedin.com/in/walleen-ates-a63287245?original_referer=https%3A%2F%2Fwww.google.com%2F">Profile</a></p>
+    </nav>
+    <div className="img">
+      
+    <img className="img1"src="https://images.moviesanywhere.com/45589cb573be13bb984b078ed3e1cf9e/a0652686-b625-4b41-bdf1-f32c3d9471a6.jpg"/>
+    <img className="img2" src="https://m.media-amazon.com/images/M/MV5BMTUwNjUxMTM4NV5BMl5BanBnXkFtZTgwODExMDQzMTI@._V1_.jpg"/>
+    <img className="img3" src="https://m.media-amazon.com/images/M/MV5BMzkzMmU0YTYtOWM3My00YzBmLWI0YzctOGYyNTkwMWE5MTJkXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"/>
+    <img className="img4" src="https://m.media-amazon.com/images/M/MV5BNzE2NzgzYzAtMzQ0OC00Zjc3LTkwYjUtMzlkMDBiNWMzZjYwXkEyXkFqcGdeQXVyMzg1ODQ4OQ@@._V1_.jpg"/>
+    <img className="img5" src="https://m.media-amazon.com/images/M/MV5BNjRjM2U0M2MtNzQ2MS00MDJmLWE4OWQtZjlhYTNkMjRmMTgxXkEyXkFqcGdeQXVyMTYwMDIyNzk1._V1_.jpg"/>
+    
+    </div>
+    <footer>
+  <p>Author: Ates   </p>
+</footer>
+    </main>
+    
+  );
+}
